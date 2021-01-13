ca62c3e5d4c1ab782b22a448bfd80e8b4431ab19
Antoine Benoit <antoine.benoit@epita.fr>
2021-01-13 16:10:25 +0100

    J'ai ajouté l'envoi de mail que j'ai pas pu tester parce que j'ai pas trouver les lignes à modifier dans le fichier php.ini

--- biblio_groupe14_etape4/contact.php
+++ biblio_groupe14_etape4/contact.php
@@ -11,9 +11,9 @@ require "head.php";
 <fieldset>
               <legend><br><br><div class="contacter" align="center"><b>Nous contacter</b></div></legend>
                   <label for="name">Nom :</label>
-                  <input type="text" id="name" name="client_name" autofocus required>
+                  <input type="text" id="name" name="name" autofocus required>
                   <label for="firstname">Prénom :</label>
-                  <input type="text" id="firstname" name="client_firstname"  required>
+                  <input type="text" id="first_name" name="first_name"  required>
                   <br>
                   <br>
                   
@@ -26,19 +26,19 @@ require "head.php";
                   <br>
                   
                   <label for="mail">Courriel :</label>
-                  <input type="mail" id="mail" name="client_mail" required><br>
+                  <input type="mail" id="mail" name="mail" required><br>
                   
                   <br>
-                  <label for="duration">Pourquoi nous contacter  ? <label>
-                  <select>
-                  <option value="1y">Livre en mauvais état</option>
-                  <option value="2y">Il n'y pas assez de livres sur BDbliothèque</option>
-                  <option value="5y">Autres</option>
+                  <select name="why_contact" required>
+                  <label for="why_contact">Pourquoi nous contacter  ? </label>
+                  <option value="livre en mauvais état">Livre en mauvais état</option>
+                  <option value="manque de livres">Il n'y pas assez de livres sur BDbliothèque</option>
+                  <option value="autre">Autres</option>
                   </select><br>
                   
                   <br>
-                  <label for"addreq">Demmandes particulières :</label><br>
-                  <textarea id="addreq" placeholder="Entrez ici vos demmandes"></textarea>
+                  <label for"addreq">Demmande particulière :</label><br>
+                  <textarea id="addreq" name="addreq" placeholder="Entrez ici votre demmande"></textarea>
                   <br>
                   <button type="reset">Tout effacer</button><br><br>
                   
---
+++ biblio_groupe14_etape4/receive.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+require "head.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $name = $_POST["name"];
+        $first_name = $_POST["first_name"];
+        $type = $_POST["type"];
+        $mail = $_POST["mail"];
+        $addreq = $_POST["addreq"];
+        $why_contact = $_POST["why_contact"];
+        $req=$why_contact;
+        if($why_contact=="autre")
+        {
+			$req=$addreq;
+		}
+        echo "<div align='center' class='requete'>Bonjour <b>$first_name $name</b> votre requête pour: <i>".$req."</i> a bien été transmise aux responsables de <b>BDbliothèque</b>.</div>";
+        $to = 'moi@domaine.com';
+        
+        $msg  = 'Bonjour,'."\r\n\r\n";
+		$msg .= 'Ce mail a été envoyé depuis monsite.com par '.$firts_name.' '.$name."\r\n\r\n";
+		$msg .= 'Voici le message qui vous est adressé :'."\r\n";
+		$msg .= '***************************'."\r\n";
+		$msg .= $addreq."\r\n";
+		$msg .= '***************************'."\r\n";
+		
+		$headers = 'From: '.$first_name.$name.' <'.$mail.'>'."\r\n\r\n";
+		
+		mail($to, $why_contact, $msg, $headers);
+        
+     }
+?>
+</html>
